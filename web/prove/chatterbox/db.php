1a430115d510b28c114d0ed2fbddb39f82879fb2
Anthony <aengland94@gmail.com>
2018-02-10 13:15:39 -0700

    require db test

---
+++ web/prove/chatterbox/db.php
@@ -0,0 +1,38 @@
+<?php
+   $currentDB = NULL;
+
+   function getDB() 
+   {
+      $db = NULL;
+
+      try 
+      {
+         $dbUrl = getenv('DATABASE_URL');
+
+         $dbopts = parse_url($dbUrl);
+         $dbHost = $dbopts["host"];
+         $dbPort = $dbopts["port"];
+         $dbUser = $dbopts["user"];
+         $dbPassword = $dbopts["pass"];
+         $dbName = ltrim($dbopts["path"],'/');
+
+         // PDO connection
+         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+         $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+      }  catch (PDOException $ex) 
+      {
+         echo "Error! Cannot connect to DB because: $ex";
+
+         die();
+      }
+
+      return $db;
+   }
+
+   function connectToDB()
+   {
+      $currentDB = getDB();
+   }
+?>

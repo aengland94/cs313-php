30f74015388dc4a294059d658d0c6f7414e2bab1
Anthony <aengland94@gmail.com>
2018-02-10 13:26:38 -0700

    fixed css link

--- web/prove/chatterbox/profile.php
+++ web/prove/chatterbox/profile.php
@@ -19,7 +19,7 @@
         <title>My Page</title>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-   	    <link rel="stylesheet" type="text/css" href="../css/basic.css"/>
+   	    <link rel="stylesheet" type="text/css" href="../../css/basic.css"/>
         <!-- JQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <!-- Popper -->

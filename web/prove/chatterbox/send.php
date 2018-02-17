e690488d7f148498d6b8ab1d82e01d298232d274
Anthony <aengland94@gmail.com>
2018-02-16 19:05:42 -0700

    send message test12

--- web/prove/chatterbox/send.php
+++ web/prove/chatterbox/send.php
@@ -2,6 +2,9 @@
    session_start();
    require "db.php";
 
+   if (!isset($_SESSION['user_id']))
+      header('Location: login.php');
+
    insertMessage($_SESSION['user_id'], $_SESSION['contact_id'], $_POST['message_text']);
 
    header('Location: profile.php');

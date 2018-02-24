deff087e71eb7977f44573987a135e9514424fd7
Anthony <aengland94@gmail.com>
2018-02-23 20:22:49 -0700

    basic style removed on profile

--- web/prove/chatterbox/profile.php
+++ web/prove/chatterbox/profile.php
@@ -20,7 +20,6 @@
         <title><?php echo $_SESSION['display_name']; ?></title>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-   	    <link rel="stylesheet" type="text/css" href="../../css/basic.css"/>
         <link rel="stylesheet" type="text/css" href="chatterbox.css"/>
         <!-- JQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
